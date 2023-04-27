84b5487596e67a11ed86a98005ef4737dd816031
Sami Omidi <samiomidi@hotmail.com>
2023-04-27 20:20:34 +0430

    drag and drop styles

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -38,9 +38,9 @@ const SingleTodo = ({ index, todo, todos, setTodos }: Props) => {
   };
   return (
     <Draggable draggableId={todo.id.toString()} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <form
-          className="todos__single"
+          className={`todos__single ${snapshot.isDragging ? "darg" : ""}`}
           onSubmit={(e) => editHandler(e, todo.id)}
           {...provided.dragHandleProps}
           {...provided.draggableProps}

2151d62acfeb7b4618a80ffa5e5d91e6a0bd65e2
Sami Omidi <samiomidi@hotmail.com>
2023-04-30 17:29:48 +0430

    backdrop comp update

--- src/components/ui/Backdrop.js
+++ src/components/ui/Backdrop.js
@@ -1,6 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import classes from "./Backdrop.module.css";
+
 function Backdrop(props) {
   return ReactDOM.createPortal(
     <div className="backdrop" onClick={props.onClick}>
